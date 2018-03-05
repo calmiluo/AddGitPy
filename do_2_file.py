de6c86fef7fd9ae7542a74cf082b259c0ffc8aaf
tongluo <tongluo.cq@gmail.com>
2018-03-05 12:03:42 -0800

    squash 2

---
+++ do_2_file.py
@@ -0,0 +1,12 @@
+# after the do_1_job commit and push under new branch
+# the pull request will added in member's github, press to make the PR
+# the member will add a new branch and a new file
+
+# when push 1st file, upstream is still not set. maybe default so that the PR will display in TEAM.
+
+# this 2nd file is record of what happen after 1st commit | push
+
+# this 2nd file can be also push without force
+
+
+# this line is added after it's become old.
